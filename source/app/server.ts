a806897f27101b17a6e37c530079c48013b9ed3a
snigbar <snigbar@outlook.com>
2023-11-24 18:26:00 +0600

    connection to db

--- source/app/server.ts
+++ source/app/server.ts
@@ -1,5 +1,16 @@
+import mongoose from 'mongoose'
 import app from './app'
+import config from './config/config'
 
-app.listen('5000', () => {
-  console.log('Server is running on port 5000')
-})
+async function main() {
+  try {
+    await mongoose.connect(config.url as string)
+    app.listen(config.port, () => {
+      console.log(`Running on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
